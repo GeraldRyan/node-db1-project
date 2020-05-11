5caeec5bc360042a0690c1334c0fbaed01721325
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-11 17:14:58 -0500

    got knex working

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require('express').Router()
 const db = require("../data/dbConfig.js");
+const knex = require('knex');
 
 const server = express();
 
@@ -10,6 +11,14 @@ server.get("/", (req,res)=>{
   res.status(200).json("You're in")
 })
 
+server.get("/accounts", (req,res)=>{
+  db.select("*").from('accounts').then(accs=>{
+    res.status(200).json(accs)
+  }).catch(err=>{
+    console.log(err)
+    res.status(500).json("Internal Server Error")
+  })
+})
 
 
 
