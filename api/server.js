54b5a3884eb2f684618a117791b4af482ac24df0
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-11 16:59:13 -0500

    got the server working

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
+const router = require('express').Router()
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req,res)=>{
+  res.status(200).json("You're in")
+})
+
+
+
+
+
 
 module.exports = server;
